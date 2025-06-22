eed4c47137c914b06f2a410413e9433cbcb47bc6
VishalxVG <avatarvishal1551@gmail.com>
2025-06-22 20:37:23 +0530

    Code Skeleton Structure Almost Complete

--- start.c
+++ start.c
@@ -4,6 +4,24 @@
 #include <string.h>
 #include "my_getline.h"
 
+/*
+
+? POINTER TERMINOLOGIES
+/ *p → dereference operator: accesses the value stored at the memory address p points to.
+(It does not "store the address" — p stores the address; *p accesses the value at that address.)
+
+ int *p; → declares p as a pointer to an integer.
+(So, p stores the address of an integer.)
+
+/ *p = 42; → dereferencing and assignment: stores the value 42 at the memory location p points to.
+(You're assigning to the value pointed to by p.)
+
+&x → gets the address of the pointer variable p.
+(If you meant &x, where x is an int, then &x would give you the address of x, which can be assigned to p.)
+
+
+*/
+
 // Creates a struct to hold the state of the user input
 typedef struct
 {
@@ -13,6 +31,30 @@ typedef struct
 } InputBuffer;
 
 // Allocates a new InputBuffer in heap memory(so it is persistant even after function call) , and initializes its fields to zero/null and return it.
+
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+} MetaCommandResult;
+
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+    StatementType type;
+} Statement;
+
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -22,6 +64,48 @@ InputBuffer *new_input_buffer()
 
     return input_buffer;
 }
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    // strncmp : compares the first 6 charaters of the buffer
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    else if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select. \n");
+        break;
+    }
+}
+
 // simply prints "db >" on the terminal
 void print_prompt() { printf("db > "); }
 
@@ -57,14 +141,27 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized commnad '%s' \n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
         {
-            printf("Unrecofnized commad '%s' .\n", input_buffer->buffer);
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s' .\n", input_buffer->buffer);
+            continue;
         }
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
