15633acaefc67a330ea667c363ce5703e3fe814a
VishalxVG <avatarvishal1551@gmail.com>
2025-06-23 01:39:20 +0530

    Started building the structure for storing data

--- start.c
+++ start.c
@@ -3,6 +3,10 @@
 #include <stdbool.h>
 #include <string.h>
 #include "my_getline.h"
+#include <stdint.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 
 /*
 
@@ -10,7 +14,7 @@
 / *p → dereference operator: accesses the value stored at the memory address p points to.
 (It does not "store the address" — p stores the address; *p accesses the value at that address.)
 
- int *p; → declares p as a pointer to an integer.
+int *p; → declares p as a pointer to an integer.
 (So, p stores the address of an integer.)
 
 / *p = 42; → dereferencing and assignment: stores the value 42 at the memory location p points to.
@@ -22,6 +26,66 @@
 
 */
 
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+// COMPACT REPRESENTAION OF A ROW
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// Code to convert to and from compact serialization
+void serialize_row(Row *source, void *destination)
+{
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row *destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// A table structure that points to pages of rows and keeps tracks
+// of how many rows there are
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct
+{
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
+
+//- START FROM HERE ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Read/write memory for particualr row
+void *row_slot(Table *table, uint32_t row_num)
+{
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = table->pages[page_num];
+    if (page == NULL)
+    {
+        // Alocate memory only when we try to access
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
 // Creates a struct to hold the state of the user input
 typedef struct
 {
@@ -29,8 +93,6 @@ typedef struct
     size_t buffer_length; // size of buffer
     ssize_t input_length; // length of input string
 } InputBuffer;
-
-// Allocates a new InputBuffer in heap memory(so it is persistant even after function call) , and initializes its fields to zero/null and return it.
 
 typedef enum
 {
@@ -41,6 +103,7 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -53,8 +116,10 @@ typedef enum
 typedef struct
 {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
+// Allocates a new InputBuffer in heap memory(so it is persistant even after function call) , and initializes its fields to zero/null and return it.
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -83,6 +148,15 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+            input_buffer->buffer, "insert %d %s %s",
+            &(statement->row_to_insert.id),
+            statement->row_to_insert.username,
+            statement->row_to_insert.email);
+        if (args_assigned < 3)
+        {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     else if (strcmp(input_buffer->buffer, "select") == 0)
